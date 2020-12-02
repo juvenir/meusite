360c2d557cf33f14a47d838347dbaf9cd47b55cc
juvenir teixeira <juvenirtx@gmail.com>
2020-12-02 15:18:29 -0300

    adicionando slideshow

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-$(function(){
- $("a").attr('href', 'http://www.google.com.br');
-  });
+var slideItem = 0;
+window.onload = function() {
+    setInterval(passarSlide, 5000);
+    
+    var slidewidth = document.getElementsById("slideshow").offsetWidth;
+    var objs = document.getElementsByClassName("slide");
+    for(var i in objs) {
+        objs[i].style.width = slidewidth;
+    }
+}
+function passarSlide() {
+    var slidewidth = document.getElementsById("slideshow").offsetWidth;
+    
+    if(slideItem >= 3) {
+        slideItem = 0;
+    } else{
+        slideItem++;
+    }
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+}
