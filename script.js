ee1c8211db0002afa61d9ebf70add857486c2c64
juvenir teixeira <juvenirtx@gmail.com>
2020-12-03 12:34:40 -0300

    responsive for tablet

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 var slideItem = 0;
-window.onload = function() {
+window.onload = function () {
     setInterval(passarSlide, 5000);
     var slidewidth = document.getElementById("slideshow").offsetWidth;
     var objs = document.getElementsByClassName("slide");
-    for(let item of objs) {
+    for (let item of objs) {
         item.style.width = slidewidth;
     }
 }
+
 function passarSlide() {
     var slidewidth = document.getElementById("slideshow").offsetWidth;
-    
-    if(slideItem >= 3) {
+
+    if (slideItem >= 3) {
         slideItem = 0;
-    } else{
+    } else {
         slideItem++;
     }
-    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
+
 function mudarSlide(pos) {
     slideItem = pos;
     var slidewidth = document.getElementById("slideshow").offsetWidth;
-    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 
 }
